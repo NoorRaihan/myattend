1dc80f26a68e3dce44ec7158beabbfbad64f3ce7
Luq4dmin <luqmanmegaman96@gmail.com>
2024-01-29 10:38:59 +0800

    Fix course details in the drawer, add a button to close drawer

--- src/main/webapp/resources/course.js
+++ src/main/webapp/resources/course.js
@@ -74,7 +74,7 @@ function courseDetails(id) {
         $("#detailBG").removeClass(
           "bg-gradient-to-br from-green-300 to-emerald-500 from-lime-300 to-lime-500 from-purple-300 to-purple-500 from-blue-300 to-blue-500 from-cyan-300 to-cyan-500 from-fuchsia-300 to-fuchsia-500 from-pink-300 to-pink-500 from-yellow-300 to-yellow-500 from-orange-300 to-orange-500 from-red-300 to-red-500"
         );
-        $("detailBG").addClass(response.data[0].course.colorConfig);
+        $("#detailBG").addClass(response.data[0].course.colorConfig);
         $("#courseName").html(response.data[0].course.course_name);
         $("#courseCode").html(response.data[0].course.course_code);
         $("#courseLecturer").html(response.data[0].course.course_lecturer);
