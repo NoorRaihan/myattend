ed72feb63124279d99af88a984bbfe14e0ce35f6
Luq4dmin <luqmanmegaman96@gmail.com>
2024-02-07 00:51:42 +0800

    Remove status residue in js

--- src/main/webapp/resources/utility.js
+++ src/main/webapp/resources/utility.js
@@ -8,16 +8,6 @@ $(document).ready(function () {
     var id = $(this).data("id");
     $("#roleId").val(id);
   });
-
-  $(document).on("click", ".statusEdit", function () {
-    var id = $(this).data("id");
-    statusDetails(id);
-  });
-
-  $(document).on("click", ".statusDelete", function () {
-    var id = $(this).data("id");
-    $("#statusId").val(id);
-  });
 });
 
 function roleDetails(id) {
@@ -34,30 +24,6 @@ function roleDetails(id) {
       } else {
         $("#role_id").val(response.data.id);
         $("#role_name").val(response.data.role_name);
-      }
-    },
-    error: function (response) {
-      let msg = "(" + response.respCode + ") " + response.respMessage;
-      $("#alertMsg").html(msg);
-      $("#alert").show().delay(5000).fadeOut();
-    },
-  });
-}
-
-function statusDetails(id) {
-  $.ajax({
-    method: "GET",
-    url: "",
-    data: { status_id: id },
-    dataType: "json",
-    success: function (response) {
-      if (response.respStatus == "error") {
-        let msg = "(" + response.respCode + ") " + response.respMessage;
-        $("#alertMsg").html(msg);
-        $("#alert").show().delay(5000).fadeOut();
-      } else {
-        $("#status_id").val(response.data.id);
-        $("#status_name").val(response.data.status_name);
       }
     },
     error: function (response) {
